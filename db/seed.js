ddd1ec99d2045bbfbd333945b7fcbf6308d13c97
Josh Ford <thisisjoshford@gmail.com>
2020-03-31 22:36:17 -0700

    data helpers

---
+++ db/seed.js
@@ -0,0 +1,12 @@
+const User = require('../lib/models/User');
+const chance = require('chance').Chance();
+
+
+module.exports = async({ usersToCreate = 10 } = {}) => {
+
+  const user = await User.create([...Array(usersToCreate)].map(() => ({
+    name: chance.name(),
+    passwordHash: chance.hash(),
+    profilePhotoUrl: 'http://www.placekitten.com/200/200'
+  })));
+};
