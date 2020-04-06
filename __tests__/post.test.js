1d5a5a33ccc0ffdbf432e950aea0a8544aa9e8c3
Josh Ford <thisisjoshford@gmail.com>
2020-04-05 21:52:00 -0700

    updates a caption by id

--- __tests__/post.test.js
+++ __tests__/post.test.js
@@ -42,7 +42,7 @@ describe('post routes', () => {
     return request(app)
       .get(`/api/v1/post/${post._id}`)  
       .then(res => {
-        console.log(res.body);
+        // console.log(res.body);
         expect(res.body)
           .toEqual({
             _id: post._id.toString(),
@@ -54,4 +54,24 @@ describe('post routes', () => {
           });
       });
   });
+
+  it('gets a post by id and updates it', async() => {
+    const post = await getPost();
+    return getAgent()
+      .patch(`/api/v1/post/${post._id}`)  
+      .send({ caption: 'woah!'})
+      .then(res => {
+        console.log(res.body);
+        expect(res.body)
+          .toEqual({
+            _id: post._id.toString(),
+            user: expect.any(String),
+            photoUrl: post.photoUrl,
+            caption: 'woah!',
+            tags: post.tags,
+            __v:0
+          });
+      });
+  });
+
 });
